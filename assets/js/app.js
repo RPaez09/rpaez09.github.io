dfefa1d6479fa0a375f4c5a3e777bcccdfd77ca9
Richard Paez <Richardp809@gmail.com>
2017-11-16 17:17:28 -0500

    Navbar now shrinks and grows with scrolling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
 /* Richard Paez | 2017 */
+
+var nav = document.getElementById("navigation-main");
+var navExpanded = true;
+
+window.addEventListener('scroll', function(e) {
+    
+    if( window.scrollY >= 20 && navExpanded == true ){
+        nav.style.paddingTop = 0;
+        nav.style.paddingBottom = 0;
+        navExpanded = false;
+    }
+
+    else if ( window.scrollY == 0 && navExpanded == false ){
+        nav.style.paddingTop = "15px";
+        nav.style.paddingBottom = "15px";
+        navExpanded = true;
+    }
+
+});
